de509ff36adeee048e6bf0df137a2aac3f04e71c
bastab00 <143991112+bastab00@users.noreply.github.com>
2024-08-15 08:18:15 +0000

    stack

---
+++ 01_basics/02_stacks_heaps.js
@@ -0,0 +1,21 @@
+// // stacks (Primitive) // heaps (non primitive)
+
+// let myName = "Bastab"
+// let nickname = myName
+// nickname = "Baby"
+
+// console.log(myName);
+// console.log(nickname);
+
+// let name = {
+//     myName1: "Basu",
+//     email: "bastab@gmail.com"
+// }
+// console.log(name);
+
+// let name2 = name
+
+// name2.email = "njfhf@gmail.com"
+// console.log(name);
+// console.log(name2);
+
