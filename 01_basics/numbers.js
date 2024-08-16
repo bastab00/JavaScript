92ce585a4e3605db1b2e4f81c825a1ebeaeadc68
bastab00 <143991112+bastab00@users.noreply.github.com>
2024-08-16 08:53:26 +0000

    nos and math

---
+++ 01_basics/numbers.js
@@ -0,0 +1,40 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(200)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.98457
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreads = 10000000
+console.log(hundreads.toLocaleString('en-IN'))
+
+
+// *********************    maths    ************************
+
+console.log(Math);
+console.log(Math.abs(-4545));
+console.log(Math.round(4.35));
+console.log(Math.ceil(4.22));
+console.log(Math.floor(5.2));
+console.log(Math.max(6, 8, 2));
+
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
