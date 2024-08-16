5ed8f33fce88821f93c5d17bb1302f498cde7d73
bastab00 <143991112+bastab00@users.noreply.github.com>
2024-08-16 09:22:27 +0000

    dates

---
+++ 01_basics/dates.js
@@ -0,0 +1,34 @@
+// let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate); // object
+// console.log(typeof Date); // function
+
+// let  myCreatedDate = new Date(2023, 0 , 25, 6 , 6); // mothns start from 0th Index
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate1 = new Date("01-05-2003")
+// console.log(myCreatedDate1.toLocaleString());
+
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp); // answers will be in milisec as it is easier to compare
+// console.log(myCreatedDate1.getTime()); // gives milisec
+// console.log(Math.floor(Date.now()/1000)); //convert milisec to sec
+
+
+const myDate = new Date()
+
+console.log(myDate.getMonth() + 1); // so it corrects the indexing
+
+
+
+
+
+
+
+
+
+
