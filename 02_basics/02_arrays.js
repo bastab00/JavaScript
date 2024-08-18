50df56f92066d44e246f6e8cdb6e9c10cea95b5a
bastab00 <143991112+bastab00@users.noreply.github.com>
2024-08-18 06:40:31 +0000

    arrays paer 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,33 @@
+const marvel_heroes = ["iron man", "thor", "spiderman"]
+const dc_heroes = ["batman", "superman", "flash"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes);
+
+// const allHeroes = [...marvel_heroes, ...dc_heroes] // spread operator
+
+// console.log(allHeroes);
+
+// const another_array = [1,2,3, [4,5,6],[1,6,[5,9]]]
+// const another_array_2 = another_array.flat(Infinity)
+
+// console.log(another_array_2);
+
+
+console.log(Array.isArray("RAJ"));
+console.log(Array.from("RAJ"));
+console.log(Array.from({name: "RAJ"})); //interesting
+
+let score1 = 100
+let score2 = 200
+
+console.log(Array.of(score1, score2));
+
+
+
+
