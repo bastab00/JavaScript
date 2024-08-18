178cb940ec8348bd5a5128bd387a6b9928fafb9c
bastab00 <143991112+bastab00@users.noreply.github.com>
2024-08-18 06:07:17 +0000

    arrays part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,43 @@
+const arr = [0, 1 , 2, 3]
+console.log(arr[2]);
+
+// const heroes = ["iron man", "cap", 'thor']
+// console.log(heroes[2]);
+
+// const arr2 = new Array(1,2,3154,56)
+// console.log(arr2);
+
+// Array Methods
+
+// arr.push(6)
+// arr.push(65)
+// arr.pop()
+
+// arr.unshift(9)
+
+// arr.shift()
+
+// // console.log(arr.includes(6));
+// // console.log(arr.indexOf(9));
+
+// const newArr = arr.join()
+
+// console.log(arr);
+// console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A", arr);
+
+const myn1 = arr.slice(1,3)
+console.log(myn1);
+
+console.log("B", arr);
+
+const myn2 = arr.splice(1, 3)
+
+console.log("C" , arr);
+console.log(myn2);
+
+
+ 
