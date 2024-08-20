38e24613b2e3d540d0578029eb9239621572e6a9
bastab00 <143991112+bastab00@users.noreply.github.com>
2024-08-20 05:58:46 +0000

    arrow

--- 03_basics/arrow.js
+++ 03_basics/arrow.js
@@ -3,8 +3,44 @@ const user = {
     price: 99,
 
     welcomeMessage: function(){
-        console.log(`${this.username}`);       
+        console.log(`${this.username}`);    
+        console.log(this);
+           
     } 
 }
 
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = 'sam'
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this); // only works inside object not in functions
+    
+// }
+
+// chai()
+
+
+// const chai = () => {         // arrow function
+//     let username = "Basu"
+//     console.log(this);    
+// }
+// chai()
+
+
+
+// const addTwo = (num1, num2) => {         // normal arrow function
+//     return num1 + num2 
+// }
+
+// const addTwo = (num1, num2) => num1 + num2          // implicite return (no need to use parenthesis)
+
+// const addTwo = (num1, num2) => (num1 + num2)
+
+const addTwo = (num1, num2) => ({username: "Basu"})
+
+console.log(addTwo(3, 4))
+
