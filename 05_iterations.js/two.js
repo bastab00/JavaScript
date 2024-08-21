fba14a98ff362ec4c274da6f032bb0d1f9f5e706
bastab00 <143991112+bastab00@users.noreply.github.com>
2024-08-21 12:16:25 +0000

    while loop

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,36 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// const myArray = [38, 98, 62, 54]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+// const myArray = [38, 98, 62, 54] 
+
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(myArray[arr]);
+//     arr++
+// }
+
+
+// let score = 12
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+// } while (score < 15 );
+
+
+
+
