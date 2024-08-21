0a5c929fad19c96677972fcba47962b197b26005
bastab00 <143991112+bastab00@users.noreply.github.com>
2024-08-21 12:59:03 +0000

    maps and for of

---
+++ 05_iterations.js/three.js
@@ -0,0 +1,49 @@
+// for of
+
+
+
+// const arr = [1,2,3,4,5,6]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const st = "Hello Baby"
+
+// for (const str of st) {
+//     if (str == ' ' ) {
+//         break
+//     }
+//     console.log(`each ch ${str}`);   
+// }
+
+// ******************** MAP ***************************
+
+// const map = new Map()
+
+// map.set('IN', "INDIA")
+// map.set('USA', "UNITED STATES OF AMERICA")
+// map.set('FR', "FRANCE")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key , '=>',  value);
+    
+// }
+
+
+
+// const myObj = {
+//     game1 = "NFS"
+//     game2 = "GTA"
+// }
+
+// for (const [key, value] of myObj) {
+//     console.log(key, value);
+    
+// }
+
+
+
